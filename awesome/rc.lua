f8f953be853a3d62feda4c492e4cdc3edcf16187
Max Bozzi <mjb@mbozzi.com>
2016-03-19 19:39:33 -0400

    Fix syntax error.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -258,7 +258,7 @@ globalkeys = awful.util.table.join(
                   mypromptbox[mouse.screen].widget,
                   awful.util.eval, nil,
                   awful.util.getdir("cache") .. "/history_eval")
-              end),
+              end)
 )
 
 clientkeys = awful.util.table.join(
