2a165ce153219d334936229ba922fb01123f401b
Max Bozzi <mjb@mbozzi.com>
2016-11-07 20:12:30 -0500

    Stop updating the Gimp run-control files.
    Add some quotes.

--- emacs/start-up-quotes.el
+++ emacs/start-up-quotes.el
@@ -1,27 +1,20 @@
-"\"If I had asked people what they wanted, they would have said
+("\"If I had asked people what they wanted, they would have said
 'faster horses.'\"
   -- Henry Ford"
 
-"Is that SC State?"
+ "\"Is that SC State?\""
 
-"I keep trying and falling, because falling is easy -- but it
-only takes you down."
+ "\"I keep trying and falling, because falling is easy -- but it
+only takes you down.\""
 
-"There is always going to be a difference between who you are and
-who you want to be.  Progress is always in one direction.  While
-you fail to move as fast as you can in that direction, you are
-failing to live."
+ "\"Isn't language cool?\""
 
-"All that remains is to compel myself to enjoy being scared."
-
-"Isn't language cool?"
-
-"Definite answers are expected excepting the case of an unknown
+ "\"Definite answers are expected excepting the case of an unknown
 fact.  In other cases, indefinite answers indicate either
 unwillingness to think about the question, or unwillingness to
-answer honestly."
+answer honestly.\""
 
-"The time in which I write... has a horribly swollen belly, it
+ "\"The time in which I write... has a horribly swollen belly, it
 carries in its womb a national catastrophe... Even an
 ignominious issue remains something other and more normal than
 the judgment that now hangs over us, such as once fell on Sodom
@@ -41,68 +34,53 @@ to despair.  When will she reach the bottom of the abyss?  When,
 out of uttermost hopelessness --- a miracle beyond the power of
 belief --- will the light of hope dawn?  A lonely man folds his
 hands and speaks: \"God be merciful to thy poor soul, my friend,
-my Fatherland!\""
+my Fatherland!\"\""
 
-"We empower them -- they do not control us.
+ "\"We empower them -- they do not control us.
 
 If you want peace, cultivate peace in your house.  If you want
 respect, respect others.
 
 You have it in in your power with every thought, relationship,
 investment, and action to immediately and permanently change your
-world."
+world.\""
 
-"Lucky girl
-on board a ship."
+ "\"Lucky girl
+on board a ship.\""
 
-"If you want respect, respect others.  If you want peace, then
-cultivate peace in your house.  If you want war, well...."
+ "\"Do I deserve retribution from aggrieved people whose lives have
+been ruined by actions my leaders have taken without my consent?\""
 
-"Do I deserve retribution from aggrieved people whose lives have
-been ruined by actions my leaders have taken without my consent?"
+ "\"Never attribute to malice what can be explained by stupidity.\""
 
-"Let mistakes be made.  But forgive them."
+ "\"Don't walk behind me: I may not lead.  Don't walk in front of
+me; I may not follow.  Just walk beside me and be my friend.\""
 
-"Never attribute to malice what can be explained by stupidity."
+ "\"Did you see the sky that night?  It wasn't the drugs.  It wasn't
+you.\""
 
-"Don't walk behind me: I may not lead.  Don't walk in front of
-me; I may not follow.  Just walk beside me and be my friend."
+ "\"A bag of skin whose contents are flesh and blood and bone.  A
+bag of skin on a speck of dust in swirling infinity.\""
 
-"Did you see the sky that night?  It wasn't the drugs.  It wasn't
-you."
-
-"A bag of skin whose contents are flesh and blood and bone.  A
-bag of skin on a speck of dust in swirling infinity."
-
-"Everybody's situation is the same, all equally valid, regardless
+ "\"Everybody's situation is the same, all equally valid, regardless
 of breadth of experience or your own personal opinion.  Some of
 us are more innocent, but that doesn't mean that what they feel
 matters any less or that their naiveté indicates they don't feel
-as sharply as anyone else."
+as sharply as anyone else.\""
 
-"What has to go through someone's mind to compel them to drive a
-truck into a crowd of hundreds of celebrating people, crushing
-them in the streets?"
-
-"Perhaps god is the greatest, but if I am supposed to do this,
-then I want no part of it."
-
-"Can somebody reconcile the demands of religion with the reality
-of life?"
-
-"It seems to me that anything that does this is necessarily
+ "\"It seems to me that anything that does this is necessarily
 self-consistent with our perception of the world and ourselves.
 True faith is not belief in something you're simply told, but
 trust in the truth of what you percieve.  Belief clings, but
-faith is a plunge."
+faith is a plunge.\""
 
-"The present phase of human thought and history... almost compels
+ "\"The present phase of human thought and history... almost compels
 us to face reality with open minds, and you can only know God
 through an open mind just as you can only see the sky through a
 clear window.  You will not see the sky if you have covered the
-glass with blue paint."
+glass with blue paint.\""
 
-"We must here make a clear distinction between belief and faith,
+ "\"We must here make a clear distinction between belief and faith,
 because, in general practice, belief has come to mean a state of
 mind which is almost the opposite of faith.  Belief, as I use the
 word here, is the insistence that the truth is what one would
@@ -113,4 +91,19 @@ opening of the mind to the truth, whatever it may turn out to be.
 Faith has no preconceptions; it is a plunge into the unknown.
 Belief clings, but faith lets go.  In this sense of the word,
 faith is the essential virtue of science, and likewise of any
-religion that is not self-deception."
+religion that is not self-deception.\""
+
+ "Arguing that you don't care about the right to privacy because
+you have nothing to hide is no different than saying you don't
+care about free speech because you have nothing to say."
+
+ "Life in general becomes far more interesting when you take away
+unexplainable conclusions."
+
+ "We shall not cease from exploration,
+   and the end of all our exploring
+      will be to arrive where we
+      started and know the place
+        for the first time.
+
+-- T.S. Eliot")
